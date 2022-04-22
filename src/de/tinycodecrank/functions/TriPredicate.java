b18203d678d316bda1bb5ddd466ea459ec45eb8d
tinycodecrank <tinycodecrank@gmail.com>
2022-04-22 09:29:46 +0200

    add MultiPredicate, improve TriPredicate

--- src/de/tinycodecrank/functions/TriPredicate.java
+++ src/de/tinycodecrank/functions/TriPredicate.java
@@ -1,5 +1,7 @@
 package de.tinycodecrank.functions;
 
+import java.util.Objects;
+
 import de.tinycodecrank.functions.applicable.ApplicableLeft;
 import de.tinycodecrank.functions.applicable.ApplicableRight;
 
@@ -7,6 +9,23 @@ import de.tinycodecrank.functions.applicable.ApplicableRight;
 public interface TriPredicate<T, U, V> extends ApplicableLeft<T, BiPredicate<U, V>>, ApplicableRight<V, BiPredicate<T, U>>
 {
 	boolean test(T t, U u, V v);
+	
+	default TriPredicate<T, U, V> and(TriPredicate<? super T, ? super U, ? super V> other)
+	{
+		Objects.requireNonNull(other);
+		return (T t, U u, V v) -> test(t, u, v) && other.test(t, u, v);
+	}
+	
+	default TriPredicate<T, U, V> negate()
+	{
+		return (T t, U u, V v) -> !test(t, u, v);
+	}
+	
+	default TriPredicate<T, U, V> or(TriPredicate<? super T, ? super U, ? super V> other)
+	{
+		Objects.requireNonNull(other);
+		return (T t, U u, V v) -> test(t, u, v) || other.test(t, u, v);
+	}
 	
 	@Override
 	default BiPredicate<U, V> aptFirst(T t)
