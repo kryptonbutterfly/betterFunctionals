9dfe8174f76a094cb0c31f69eb3a2efd2868a288
tinycodecrank <tinycodecrank@gmail.com>
2022-05-16 11:27:41 +0200

    update to jdk 17

---
+++ src/module-info.java
@@ -0,0 +1,23 @@
+module Functional
+{
+	exports de.tinycodecrank.functions.float_.applicable;
+	exports de.tinycodecrank.functions.char_.applicable;
+	exports de.tinycodecrank.functions.byte_.applicable;
+	exports de.tinycodecrank.functions.short_;
+	exports de.tinycodecrank.functions.void_;
+	exports de.tinycodecrank.functions.long_.applicable;
+	exports de.tinycodecrank.functions.bool_;
+	exports de.tinycodecrank.functions.int_;
+	exports de.tinycodecrank.functions.short_.applicable;
+	exports de.tinycodecrank.functions.long_;
+	exports de.tinycodecrank.functions;
+	exports de.tinycodecrank.functions.byte_;
+	exports de.tinycodecrank.functions.int_.applicable;
+	exports de.tinycodecrank.functions.applicable;
+	exports de.tinycodecrank.functions.bool_.applicable;
+	exports de.tinycodecrank.functions.double_.applicable;
+	exports de.tinycodecrank.functions.throwing;
+	exports de.tinycodecrank.functions.char_;
+	exports de.tinycodecrank.functions.double_;
+	exports de.tinycodecrank.functions.float_;
+}
